a3ab587fb316bdd4e79d5e4d3c9e095ee8b7ff35
Plopsis <kirvesmies70@hotmail.com>
2018-04-19 15:52:09 +0300

    basic debuglayer

--- objects/main.gd
+++ objects/main.gd
@@ -2,6 +2,6 @@ extends Node
 
 func _input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
-		$UI/menu/ui.visible = !$UI/menu/ui.visible
+		$ui/menu/ui.visible = !$ui/menu/ui.visible
 	if Input.is_action_just_pressed("ui_focus_prev"):
 		get_tree().reload_current_scene()
