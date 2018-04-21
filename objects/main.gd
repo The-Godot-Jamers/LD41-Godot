0004b098fdd88e6e163ab2e92a54e49400b190c1
Plopsis <kirvesmies70@hotmail.com>
2018-04-21 21:05:07 +0300

    Simple gridmap room
    debug is also hidden normally

--- objects/main.gd
+++ objects/main.gd
@@ -5,3 +5,5 @@ func _input(event):
 		$ui/menu/ui.visible = !$ui/menu/ui.visible
 	if Input.is_action_just_pressed("ui_focus_prev"):
 		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("1"):
+		$ui/debug/ui.visible = !$ui/debug/ui.visible
