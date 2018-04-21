132fcbfe11da690a0c2a6fd9830ba19979237216
Deep Gaurav <deepgauravraj@gmail.com>
2018-04-21 11:56:59 +0530

    Cam zoom added
    Camera can now zoom and rotate, use arrow keys

--- objects/tablescene/TableHandler.gd
+++ objects/tablescene/TableHandler.gd
@@ -13,8 +13,8 @@ func spawn_deck(cards):
 	var spawnpos=$CardLaybed/DeckPosition.global_transform
 	for x in range(cards):
 		var card_instance=card_scene.instance()
+		$CardLaybed/CardHolder.add_child(card_instance)
 		cards_ins.append(card_instance)
 		card_instance.global_transform=spawnpos
-		add_child(card_instance)
 		card_instance.translation.y+=x*0.1
 		card_instance.flip()
