641a953d9f820008f798a2a7f2a7eb80376322dd
jeremi360 <jeremi360@gmail.com>
2018-04-21 07:17:12 +0200

    add simple script for menu

---
+++ objects/menu.gd
@@ -0,0 +1,17 @@
+extends VBoxContainer
+
+func _ready():
+	$StartButton/start.connect("pressed", self, "_on_start")
+	$OptionsButton/options.connect("pressed", self, "_on_options")
+	$QuitButton/quit.connect("pressed", self, "_on_quit")
+
+
+func _on_start():
+	hide()
+	# add stuff to active 2d scene
+
+func _on_options():
+	pass
+
+func _on_quit():
+	get_tree().quit()
