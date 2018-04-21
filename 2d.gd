8e25c540c138bf5816efbcf4acd0fa5c208de71a
jeremi360 <jeremi360@gmail.com>
2018-04-21 20:02:31 +0200

    simple aim and add random delay for start target walk anim

--- 2d.gd
+++ 2d.gd
@@ -1,17 +1,35 @@
 extends Node
 
-export(int) var targets = 30 
+export(int) var targets = 30
 var used_pos = PoolVector2Array()
 var target_tscn = preload("res://objects/target.tscn")
+var targets_array = []
+var current_target
 onready var spawn_area = $Node2D/Ground/SpawnArea
+onready var crosshair = $Node2D/Player/Crosshair
 
 func _ready():
 	# connet to right signal
 	spawn_area.hide()
-	spawn_targets(30)
+	spawn_targets(targets)
+	_on_card_shoot(0)
 
-func _on_card_shoot(index, hit):
-	$AnimationPlayer.play("shoot")
+func _on_card_shoot(index):
+	current_target = targets_array[index]
+	crosshair.show()
+
+func _input(event):
+	if !crosshair.visible:
+		return
+		
+	if event is InputEventMouse:
+		crosshair.global_position = event.global_position
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			var hit = current_target.in_hit_box(crosshair.position)
+			current_target.shoot(hit)
+			prints("shoot", current_target.id, hit)
+			
 
 func random_pos_in_spawn_area():
 	randomize()
@@ -19,7 +37,7 @@ func random_pos_in_spawn_area():
 
 func check_distance(pos, array, distance):
 	for a in array:
-		if pos.distance_to(a) < 15:
+		if pos.distance_to(a) < distance:
 			continue
 		else:
 			return false
@@ -28,7 +46,7 @@ func check_distance(pos, array, distance):
 
 func spawn_targets(number):
 	used_pos = []
-	for i in range(0, number):
+	for i in range(0, number - 1):
 		#prints("make target number", i)
 		var pos_id = random_pos_in_spawn_area()
 		var pos = spawn_area.polygon[pos_id]
@@ -48,5 +66,6 @@ func spawn_targets(number):
 		target.position += spawn_area.polygon[pos_id]
 		#print(spawn_area.position)
 		add_child(target)
+		targets_array.append(target)
 
 		
