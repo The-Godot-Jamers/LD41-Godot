b8307320099df4d2f8d59d048063cf5e7e68d9aa
Plopsis <kirvesmies70@hotmail.com>
2018-04-21 15:33:34 +0300

    splash and card changes
    added highlighting shader for card

--- objects/card.gd
+++ objects/card.gd
@@ -4,19 +4,31 @@ extends Spatial
 # var a = 2
 # var b = "textvar"
 var tweennode
-
+var is_flipped = true
+ 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	tweennode=$Tween
 
 func flip():
-	$MeshInstance/AnimationPlayer.play("flip")
+	$Area/MeshInstance/AnimationPlayer.play("flip")
 func flip_back():
-	$MeshInstance/AnimationPlayer.play("flip back")
+	$Area/MeshInstance/AnimationPlayer.play("flip back")
 	
 
-func mouse_over():
-	$MeshInstance.get_material().set_shader_param("mouse_over", true)
-func mouse_out():
-	$MeshInstance.get_material().set_shader_param("mouse_over", false)
+func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx):
+	if event is InputEventMouseButton:
+		if is_flipped:
+			flip_back()
+		else:
+			flip()
+		is_flipped = !is_flipped
+
+
+func _on_Area_mouse_entered():
+	$Area/MeshInstance.get_surface_material(0).set_shader_param("mouse_over", true)
+
+
+func _on_Area_mouse_exited():
+	$Area/MeshInstance.get_surface_material(0).set_shader_param("mouse_over", false)
--- objects/ggj_splash.gd
+++ objects/ggj_splash.gd
@@ -1,5 +1,5 @@
 extends Spatial
 
 func _input(event):
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("mouse_left"):
 		get_tree().change_scene("res://objects/main.tscn")
