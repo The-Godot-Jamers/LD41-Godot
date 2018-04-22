3d1109d58d0a12a21566d65eb4b22a5eff1af37b
Deep Gaurav <deepgauravraj@gmail.com>
2018-04-22 17:40:55 +0530

    USing character in 2d

--- objects/target.gd
+++ objects/target.gd
@@ -11,7 +11,7 @@ func _ready():
 	var g = rand_range(0, 1)
 	randomize()
 	var b = rand_range(0, 1)
-	modulate = Color(r, g, b)
+	$AnimatedSprite.self_modulate = Color(r, g, b)
 	randomize()
 	$Timer.wait_time = rand_range(0, 2)
 	$Timer.connect("timeout", $AnimationPlayer, "play", ["walk"])
