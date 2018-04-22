721187c6d13d6d451a2aa8e37622fd77d20efbea
jeremi360 <jeremi360@gmail.com>
2018-04-22 11:19:07 +0200

    Better shooting

--- 2d.gd
+++ 2d.gd
@@ -4,12 +4,16 @@ export(int) var targets = 30
 var used_pos = PoolVector2Array()
 var target_tscn = preload("res://objects/target.tscn")
 var targets_array = []
+var targets_in_line = []
 var current_target
 onready var spawn_area = $Node2D/Ground/SpawnArea
 onready var crosshair = $Node2D/Player/Crosshair
+onready var crosshair_area = $Node2D/Player/Crosshair/Area2D
 
 func _ready():
 	# connet to right signal
+	crosshair_area.connect("area_entered", self, "_on_crosshair_arena_enter")
+	crosshair_area.connect("area_exited", self, "_on_crosshair_arena_exit")
 	spawn_area.hide()
 	spawn_targets(targets)
 	_on_card_shoot(0)
@@ -24,8 +28,25 @@ func _input(event):
 		
 	if event is InputEventMouse:
 		crosshair.global_position = event.global_position
-
+	
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			var most_front_id = 0
+			for t in targets_in_line:
+				most_front_id = max(most_front_id, t.id)
 			
+			targets_array[most_front_id].shoot(true)
+
+func _on_crosshair_arena_enter(area):
+	if area.owner in targets_array:
+		if !(area.owner in targets_in_line):
+			targets_in_line.append(area.owner)
+
+func _on_crosshair_arena_exit(area):
+	if area.owner in targets_array:
+		if area.owner in targets_in_line:
+			var t_id = targets_in_line.find(area.owner)
+			targets_in_line.remove(t_id)
 
 func random_pos_in_spawn_area():
 	randomize()
--- objects/target.gd
+++ objects/target.gd
@@ -17,26 +17,10 @@ func _ready():
 	$Timer.connect("timeout", $AnimationPlayer, "play", ["walk"])
 	$Timer.start()
 	$AnimationPlayer.connect("animation_finished", self, "_on_anim_finish")
-	$AnimatedSprite/Area2D.connect("mouse_entered", self, "_on_mouse_enter")
-	$AnimatedSprite/Area2D.connect("mouse_exit", self, "_on_mouse_exit")
 
 func _on_anim_finish(anim_name):
 	if anim_name == "miss":
 		$AnimationPlayer.play("walk")
-
-func _on_mouse_enter():
-	mouse_in = true
-
-func _on_mouse_exit():
-	mouse_in = false
-
-func _input(event):
-	if !mouse_in:
-		return
-	
-	if event is InputEventMouseButton:
-		shoot(true)
-	
 
 func shoot(hit):
 	if hit:
