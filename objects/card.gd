5500ecae58157dd35c473669b6cf8983f738b1ed
Deep Gaurav <deepgauravraj@gmail.com>
2018-04-23 23:53:21 +0530

    Fixed multiplecards

--- objects/card.gd
+++ objects/card.gd
@@ -32,11 +32,13 @@ func show(charid):
 	CharacterGenerator.makecardspr(CharacterGenerator.characters[charid],self)
 	$Tween.interpolate_property(self,"translation",Vector3(0.0,-0.3,-0.25),Vector3(0.0,-0.1,-0.25),1.0,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
 	$Tween.start()
+	lockall()
 func hide():
 	$selection.show()
 	#print("hide")
 	$Tween.interpolate_property(self,"translation",Vector3(0.0,-0.1,-0.25),Vector3(0.0,-0.3,-0.25),1.0,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
 	$Tween.start()
+	unlockall()
 func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx):
 	if event is InputEventMouseButton:
 		if $Area/MeshInstance/AnimationPlayer.is_playing() or flipping:
@@ -69,13 +71,24 @@ func _on_accept_pressed():
 	$selection.hide()
 	flip_back()
 	tablehandler.cards_ins[characterid].lockcard()
+	tablehandler.cards_ins[characterid].flippable=false
+	unlockall()
 func _on_put_down_pressed():
-	tablehandler.flip_card(characterid)
-	flip_back()
+	#tablehandler.flip_card(characterid)
+	tablehandler.cards_ins[characterid].flip()
+	tablehandler.cards_ins[characterid].is_flipped=false
 	$Tween.interpolate_property(self,"translation",Vector3(0.0,-0.1,-0.25),Vector3(0.0,-0.3,-0.25),1.0,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
 	$selection.hide()
-	
+	unlockall()
 func lockcard():
 	$Area.disconnect("input_event",self,"_on_Area_input_event")
 
-
+func unlockcard():
+	$Area.connect("input_event",self,"_on_Area_input_event")
+func lockall():
+	for x in tablehandler.cards_ins:
+		x.lockcard()
+func unlockall():
+	for x in range(tablehandler.cards_ins.size()):
+		if not(x in Globals.targets):
+			tablehandler.cards_ins[x].unlockcard()
