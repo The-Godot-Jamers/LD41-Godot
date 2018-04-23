37f9fa0175dacf4524c66d409e9976cedcd7f27f
Alexander Holland <alexander.holland@live.de>
2018-04-23 23:15:03 +0200

    made loading beautiful

--- objects/Loader.gd
+++ objects/Loader.gd
@@ -1,32 +1,23 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 var thread
 var scn
+
 func _ready():
 	thread=Thread.new()
-	$CanvasLayer/Sprite.position=get_viewport().get_texture().get_size()/2
+	#$CanvasLayer/Label.position = get_viewport().get_texture().get_size()/2
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pa
+
 func loadscn(path):
 	if $CanvasLayer/AnimationPlayer.is_playing():
 		pass
 	else:
 		fadeloader(path)
+
 func fadeloader(path):
-	scn=load(path)
-	var t=get_viewport().get_texture().get_data()
+	scn = load(path)
+	var t = get_viewport().get_texture().get_data()
 	t.flip_y()
-	var im=ImageTexture.new()
-	im.create_from_image(t)
-	$CanvasLayer/Sprite.texture=im
-	$CanvasLayer/Sprite.self_modulate.a=1
 	yield(get_tree(),'idle_frame')
 	$CanvasLayer/AnimationPlayer.play("fade")
 	get_tree().change_scene_to(scn)
-	
